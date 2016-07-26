fae866576758dcaaeebf458b2461d9debb517781
EnzoFurmenti <ducheenzo@mail.ru>
2016-07-26 21:53:40 +0300

    Initial Commit

---
+++ Dictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dictionary
+//
+//  Created by EnzoF on 26.07.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
